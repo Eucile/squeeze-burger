be18a86c9b8f5f2e0725ebc98c141e8934f34baa
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-11 11:57:38 -0800

    import connect and PropTypes, connect mapStateToProps which contains addresses state slice, define address propType, pass prop type address to Admin and Body components

--- src/components/App.js
+++ src/components/App.js
@@ -4,45 +4,41 @@ import Admin from './Admin';
 import Body from './Body';
 import Booking from './Booking';
 import Footer from './Footer';
+import TestGoogleAPI from './TestGoogleAPI';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      latitude: null,
-      longitude: null
-    }
-    this.handleNewAddress = this.handleNewAddress.bind(this);
-  }
-
-  handleNewAddress(streetAddress, city, state, zip) {
-    const addressConcat = streetAddress + city + state + zip;
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressConcat}&key=AIzaSyC45taHipsUXKJ0Pi76aupQGoe8qGNeUmI`).then(
-      response => response.json(),
-      error => console.log('An error occurred.', error)
-    ).then((json) => {
-      this.setState({
-        latitude: json.results[0].geometry.lat,
-        longitude: json.results[0].geometry.lng
-      })
-      console.log(this.state);
-      console.log('CHECK OUT THIS SWEET API:', json)
-    }, () => {
-      console.log('hi')
-    });
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <div className="App">
         <Header/>
-        <Admin/>
-        <Body/>
+        <Admin
+          addresses={this.props.mapStateToProps}
+        />
+        <Body
+          addresses={this.props.mapStateToProps}
+        />
         <Booking/>
         <Footer/>
+        <TestGoogleAPI/>
       </div>
     );
   }
+};
+
+App.propTypes = {
+  addresses: PropTypes.object
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    addresses: state.addresses
+  };
+};
+
+export default connect(mapStateToProps)(App);
