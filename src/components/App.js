edf2aa8c60ba0911aface5770d5e8564147d6c6c
Krystal Foster <kmfoster777@gmail.com>
2018-12-10 16:38:02 -0800

    add and style maplist, hero, and header components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Hero from './Hero';
+import Maps from './Maps';
+import MenuList from './MenuList';
+
 
 class App extends Component {
   render() {
@@ -7,17 +11,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <Header/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <Hero/>
+        <Maps/>
+        <MenuList/>
         </header>
       </div>
     );
