7952d867d7a27683d3e47b77ed7114cb806657d9
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-11 12:02:41 -0800

    change Maps import to Map, import PropTypes, define streetAddress, city, zipcode, timeOpen, timeClosed, lat and lng propTypes, pass propTypes lat and lng to Map component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Map from './Map';
+import PropTypes from 'prop-types';
 
-function Event() {
+function Event(props) {
   return(
-    <div>Test</div>
+    <div>
+      // KRYSTAL!!!!!! Event info goes here
+      <Map
+        lat={props.lat}
+        lng={props.lng}
+      />
+    </div>
   );
+}
+
+Event.propTypes = {
+  streetAddress: PropTypes.string,
+  city: PropTypes.string,
+  zipCode: PropTypes.string,
+  timeOpen: PropTypes.string,
+  timeClosed: PropTypes.string,
+  lat: PropTypes.string,
+  lng: PropTypes.string
 }
 
 export default Event;
