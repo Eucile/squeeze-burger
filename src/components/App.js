65bae92f2c3b6411b45a9b4c785f97663247b197
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-10 16:28:51 -0800

    Make API call from Google Maps and start setting up Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import AddForm from './AddForm';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      latitude: null,
+      longitude: null
+    }
+    this.handleNewAddress = this.handleNewAddress.bind(this);
+  }
+
+  handleNewAddress(streetAddress, city, state, zip) {
+    const addressConcat = streetAddress + city + state + zip;
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressConcat}&key=AIzaSyC45taHipsUXKJ0Pi76aupQGoe8qGNeUmI`).then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then((json) => {
+      this.setState({
+        latitude: json.results[0].geometry.lat,
+        longitude: json.results[0].geometry.lng
+      })
+      console.log(this.state);
+      console.log('CHECK OUT THIS SWEET API:', json)
+    }, () => {
+      console.log('hi')
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,6 +44,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <AddForm onNewAddress={this.handleNewAddress}/>
       </div>
     );
   }
