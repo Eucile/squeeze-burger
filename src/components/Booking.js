a540914a5e47dce61aeb4b260483f3afb08cea07
AJ Mcgladdery <amcgladd@gmail.com>
2018-12-12 10:26:43 -0800

    add new sample contact form to booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -8,7 +8,16 @@ import '../assets/styles/Booking.scss';
 
 function Booking(){
   return (
-
+    <form id="contact-form">
+           <input type="hidden" name="contact_number">
+           <label>Name</label>
+           <input type="text" name="user_name">
+           <label>Email</label>
+           <input type="email" name="user_email">
+           <label>Message</label>
+           <textarea name="contact"></textarea>
+           <input type="submit" value="Send">
+       </form>
   );
 }
 
