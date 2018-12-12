9e13914d6e78d4c64f7a4303ab1380e9b4d4e014
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-12 11:04:33 -0800

    add UPDATE_ADDRESS switch case which only returns state

--- src/reducers/adminInfoReducer.js
+++ src/reducers/adminInfoReducer.js
@@ -8,7 +8,8 @@ export default (state = {}, action) => {
       newState = Object.assign({}, state);
       newState[action.address.id] = action.address;
       return newState;
-
+    case 'UPDATE_ADDRESS':
+      return state;
     default:
       return state;
   }
