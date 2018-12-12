dfad71d7c929ca00e2cc98473886ce3274750d7a
AJ Mcgladdery <amcgladd@gmail.com>
2018-12-12 10:31:55 -0800

    set up functionality to send contact form to admin's email address

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -9,14 +9,14 @@ import '../assets/styles/Booking.scss';
 function Booking(){
   return (
     <form id="contact-form">
-           <input type="hidden" name="contact_number">
+           <input type="hidden" name="contact_number"/>
            <label>Name</label>
-           <input type="text" name="user_name">
+           <input type="text" name="user_name"/>
            <label>Email</label>
-           <input type="email" name="user_email">
+           <input type="email" name="user_email"/>
            <label>Message</label>
            <textarea name="contact"></textarea>
-           <input type="submit" value="Send">
+           <input type="submit" value="Send"/>
        </form>
   );
 }
