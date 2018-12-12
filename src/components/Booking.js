38c33c4e79daa79d524164a47e7476caf1de8ea4
AJ Mcgladdery <amcgladd@gmail.com>
2018-12-12 11:15:46 -0800

    update the booking form in booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -5,44 +5,38 @@ import '../assets/styles/Booking.scss';
 
 function Booking(){
 
-  function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onClick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 
 
   return (
     <form id="contact-form">
-      <label>About Your Event</label>
-        <div className="dropdown">
-          <button onClick={myFunction} className="dropbtn">Dropdown</button>
-          <div id="myDropdown" className="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-       <input type="hidden" name="contact_number"/>
-       <label>Name</label>
-       <input type="text" name="user_name"/>
-       <label>Email</label>
-       <input type="email" name="user_email"/>
-       <label>Message</label>
-       <textarea name="contact"></textarea>
+        <label>Date Of Event</label>
+        <input name='on' type='date' required="required" label="Date of your event" onChange={function(e, value) {}} />
+        <br/>
+        <label>Hours</label>
+        <input name="from" placeholder="from" label="Time Range" />
+        <input name="to" placeholder="to" />
+        <br/>
+        <label>Location</label>
+        <input name="location" label="Location"/>
+        <br/>
+        <label>Approx. number of Guests</label>
+         <input name="Guests" type="number" label="Approx. number of guests" />
+        <br/>
+        <label>Tell Us About Your Event</label>
+        <textarea name="details"></textarea>
+        <br/>
+        <label>Name:</label>
+        <input name="Name"/>
+        <br/>
+        <label>Phone:</label>
+        <input name="Phone" validate type='tel'/>
+        <br/>
+          <label>Email:</label>
+          <input name="Email"/>
+          <br/>
+            <label>Company:</label>
+            <input name="Company"/>
+            <br/>
        <input type="submit" value="Send"/>
       </form>
   );
@@ -51,61 +45,3 @@ window.onClick = function(event) {
 
 
 export default Booking;
-
-
-
-
-// <Row>
-//   <Col s={12}>
-//     <div className="container">
-//       <form action="mailto:amcgladd@gmail.com" method="POST" encType="text/plain">
-//         <Row>
-//           <h5>About Your Event</h5>
-//           <Input s={12} type='select' label="Event Type" defaultValue='2' name="Event Type:">
-//             <option value='Office_Lunch'>Office Lunch</option>
-//             <option value='Private_Party'>Private Party</option>
-//             <option value='Public_event'>Public event or festival</option>
-//             <option value='Wedding'>Wedding</option>
-//           </Input>
-//         </Row>
-//         <Row>
-//           <Input  s={12} name='on' type='date' required="required" label="Date of your event" onChange={function(e, value) {}} />
-//         </Row>
-//         <Row>
-//           <Input name="from" placeholder="from" s={6} label="Time Range" />
-//           <Input name="to" placeholder="to" s={6} />
-//         </Row>
-//         <Row>
-//           <Input name="location" s={12} label="Location" />
-//         </Row>
-//         <Row>
-//           <Input s={12} name="Guests" type="number" label="Approx. number of guests" />
-//         </Row>
-//         <Row>
-//           <Input s={12}  name="Paying" type='select' label="Who Is Paying?" defaultValue='1'>
-//             <option value='1'>Our Guests</option>
-//             <option value='2'>We Are</option>
-//           </Input>
-//         </Row>
-//         <Row>
-//           <Input s={12}  name="Event Details" type='textarea' placeholder="Tell Us About Your Event" />
-//         </Row>
-//         <Row>
-//           <h5>Contact Information:</h5>
-//           <Input name="Name" s={12} label="Your Name" validate><Icon>account_circle</Icon></Input>
-//         </Row>
-//         <Row>
-//           <Input s={12} name="Phone" label="Telephone" validate type='tel'><Icon>phone</Icon></Input>
-//         </Row>
-//         <Row>
-//           <Input type="email" name="email" label="Email" s={12} ><Icon>email</Icon></Input>
-//         </Row>
-//
-//         <Row>
-//           <Input s={12} name="company" placeholder="company"><Icon>home</Icon></Input>
-//         </Row>
-//         <Button type="submit" waves='light' type="submit" value="Send">button</Button>
-//       </form>
-//     </div>
-//   </Col>
-// </Row>
