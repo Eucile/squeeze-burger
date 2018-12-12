1443fc14eb0b744ee57a2facc9d9df9e2904f7f3
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-11 19:58:34 -0800

    replace props argument with destructored addresses propType and map against it

--- src/components/AdminEvent.js
+++ src/components/AdminEvent.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function AdminEvent(props) {
+function AdminEvent({ streetAddress, state, city, zipcode, date, timeOpen, timeClosed }) {
   return(
     <div>
-      
+      <p>{streetAddress}</p>
+      <p>{city}, {state} {zipcode}</p>
+      <p>DATE GOES HERE</p>
+      <p>TIMEOPEN GOES HERE</p>
+      <p>TIMECLOSE GOES HERE</p>
     </div>
   );
 }
