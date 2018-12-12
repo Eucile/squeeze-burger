4433f39e898006643e022b4aafe4dd954cdc9e86
AJ Mcgladdery <amcgladd@gmail.com>
2018-12-12 11:46:13 -0800

    finalize form functionality to send emails to admin's email

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -9,36 +9,36 @@ function Booking(){
 
   return (
     <form id="contact-form">
-        <label>Date Of Event</label>
-        <input name='on' type='date' required="required" label="Date of your event" onChange={function(e, value) {}} />
-        <br/>
-        <label>Hours</label>
-        <input name="from" placeholder="from" label="Time Range" />
-        <input name="to" placeholder="to" />
-        <br/>
-        <label>Location</label>
-        <input name="location" label="Location"/>
-        <br/>
-        <label>Approx. number of Guests</label>
-         <input name="Guests" type="number" label="Approx. number of guests" />
-        <br/>
-        <label>Tell Us About Your Event</label>
-        <textarea name="details"></textarea>
-        <br/>
-        <label>Name:</label>
-        <input name="Name"/>
-        <br/>
-        <label>Phone:</label>
-        <input name="Phone" validate type='tel'/>
-        <br/>
-          <label>Email:</label>
-          <input name="Email"/>
-          <br/>
-            <label>Company:</label>
-            <input name="Company"/>
-            <br/>
-       <input type="submit" value="Send"/>
-      </form>
+      <label>Date Of Event</label>
+      <input name='date' type='date' label="Date of your event" onChange={function(e, value) {}} />
+      <br/>
+      <label>Hours</label>
+      <input name="from" placeholder="from" label="Time Range" />
+      <input name="to" placeholder="to" />
+      <br/>
+      <label>Location</label>
+      <input name="location" label="Location"/>
+      <br/>
+      <label>Approx. number of Guests</label>
+      <input name="guests" type="number" label="Approx. number of guests" />
+      <br/>
+      <label>Tell Us About Your Event</label>
+      <textarea name="details"></textarea>
+      <br/>
+      <label>Name:</label>
+      <input required="required" name="name"/>
+      <br/>
+      <label>Phone:</label>
+      <input name="phone" validate='true'/>
+      <br/>
+      <label>Email:</label>
+      <input required="required" name="email"/>
+      <br/>
+      <label>Company:</label>
+      <input name="company"/>
+      <br/>
+      <input type="submit" value="Send"/>
+    </form>
   );
 }
 
