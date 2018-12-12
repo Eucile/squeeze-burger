1948a9f073971a93785440d7c3c7aaf37b51917b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-12 15:11:50 -0800

    change deleteSelectedAddress to remove()

--- src/actions/index.js
+++ src/actions/index.js
@@ -67,6 +67,7 @@ export function watchFirebaseEditAddressesRef() {
       });
       dispatch(receiveAddress(newAddress));
     });
+
   }
 };
 
--- src/components/AdminEvent.js
+++ src/components/AdminEvent.js
@@ -2,15 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-function AdminEvent({ onChangingSelectedEditEvent, streetAddress, state, city, zipcode, date, timeOpen, timeClosed, eventId }) {
+function AdminEvent({ onChangingSelectedEditEvent, streetAddress, state, city, zipcode, date, timeOpen, timeClose, eventId, addressState }) {
 
-function AdminEvent({ streetAddress, addressState, city, zipcode, date, timeOpen, timeClose}) {
   return(
-<<<<<<< HEAD
-    <div id={eventId}>
-      <p>{streetAddress}</p>
-      <p>{city}, {state} {zipcode}</p>
-=======
     <div className="box">
       <style>{`
         .box {
@@ -21,7 +15,6 @@ function AdminEvent({ streetAddress, addressState, city, zipcode, date, timeOpen
       <p>{date}</p>
       <p>{streetAddress}, {city}, {addressState} {zipcode}</p>
       <p>{timeOpen} to {timeClose}</p>
->>>>>>> master
       <button
         onClick={() => {
           onChangingSelectedEditEvent(eventId);
