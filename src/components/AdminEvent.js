c4dd4ae67bb89879c8bfb5956e7fa90c30fc888b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-12 11:15:45 -0800

    define onChangingSelectedEditEvent PropType, add to AdminEvent destructoring syntax and pass in button's binded onClick function

--- src/components/AdminEvent.js
+++ src/components/AdminEvent.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function AdminEvent({ streetAddress, state, city, zipcode, date, timeOpen, timeClosed }) {
+function AdminEvent({ onChangingSelectedEditEvent, streetAddress, state, city, zipcode, date, timeOpen, timeClosed, eventId }) {
+
   return(
     <div>
       <p>{streetAddress}</p>
@@ -9,18 +11,25 @@ function AdminEvent({ streetAddress, state, city, zipcode, date, timeOpen, timeC
       <p>DATE GOES HERE</p>
       <p>TIMEOPEN GOES HERE</p>
       <p>TIMECLOSE GOES HERE</p>
+      <button
+        onClick={() => {
+          onChangingSelectedEditEvent(eventId);
+        }}
+      >Edit Button</button>
     </div>
   );
 }
 
 AdminEvent.propTypes = {
+  onChangingSelectedEditEvent: PropTypes.func,
   streetAddress: PropTypes.string,
   city: PropTypes.string,
   state: PropTypes.string,
   zipcode: PropTypes.string,
   date: PropTypes.string,
   timeOpen: PropTypes.string,
-  timeClose: PropTypes.string
+  timeClose: PropTypes.string,
+  eventId: PropTypes.string
 }
 
-export default AdminEvent;
+export default connect()(AdminEvent);
