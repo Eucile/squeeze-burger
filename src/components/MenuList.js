aba69aa568c9cfa16364dcf9333e8aaabc3f9930
Krystal Foster <kmfoster777@gmail.com>
2018-12-11 20:19:01 -0800

    add symmetry to menu

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -13,29 +13,34 @@ function MenuList(){
     <div className="menu-container">
       <div className="main-menu">
         <h1>Mains</h1>
-          <div>
-          <div className="menu-title-flex">
-            <h2>Burger</h2><h2> 10.00</h2>
-          </div>
-            <p>lettuce, tomato, pickles, onion, mayo, mustard</p>
-          </div>
-          <div>
-          <div className="menu-title-flex">
-            <h2>Hotdog</h2><h2> 7.00</h2>
-          </div>
-            <p>lettuce, tomatoes, pickles, onion, mayo, mustard, relish</p>
-          </div>
-          <div>
+          <div className="menu-spacing">
             <div className="menu-title-flex">
-              <h2>Chicken sandwich</h2><h2> 9.00</h2>
+              <h3>Burger</h3><h3> 10.00</h3>
+            </div>
+              <p>lettuce, tomato, pickles, onion, mayo, mustard</p>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Hotdog</h3><h3> 7.00</h3>
+            </div>
+              <p>lettuce, tomatoes, pickles, onion, mayo, mustard, relish</p>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Chicken sandwich</h3><h3> 9.00</h3>
             </div>
             <p>bacon, mayo, avocado, onion, lettuce, tomatoes</p>
           </div>
         </div>
         <div className="sides-menu">
-        <h1>Sides</h1>
+          <h1>Sides</h1>
+          <div className="side-flex">
+          <div>
           <p>fries</p>
           <p>onion rings</p>
+          </div>
+            <p className="combo"><em>(sides included as combo with your main order)</em></p>
+          </div>
         </div>
       </div>
       <div className="menu-bottom">
