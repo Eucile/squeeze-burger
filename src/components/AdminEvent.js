694bef886db65717815d2a32e4aefc4510807a6a
Brendan Shea <bjrshea@gmail.com>
2018-12-12 19:55:30 -0800

    Edit admin results box

--- src/components/AdminEvent.js
+++ src/components/AdminEvent.js
@@ -8,20 +8,44 @@ function AdminEvent({onChangingSelectedEditEvent, streetAddress, addressState, c
       <style>{`
         .box {
           border: 1px solid #E9ECF2;
-          margin: 20px;
+          width: 800px;
+        }
+        .dateAndTime {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 20px;
+          margin-left: 20px;
+          margin-right: 20px;
+          font-size: 1rem;
+          color: #868e9e;
+        }
+        .address {
+          margin-top: 5px;
+          margin-left: 20px;
+          font-size: 1.25rem;
+          color: #49505e;
+        }
+        .button {
+          display: flex;
+          justify-content: center;
+          margin-top: 20px;
+          margin-bottom: 20px;
         }
       `}</style>
-      <p>{date}</p>
-      <p>{streetAddress}, {city}, {addressState} {zipcode}</p>
-      <p>{timeOpen} to {timeClose}</p>
-
-      <button
-        onClick={() => {
-          onChangingSelectedEditEvent(eventId);
-        }}>Edit Button</button>
+      <div className="dateAndTime">
+        <h6>{date}</h6>
+        <h6>{timeOpen} to {timeClose}</h6>
+      </div>
+      <div className="address">
+        <h5>{streetAddress}, {city}, {addressState} {zipcode}</h5>
+      </div>
+      <div className="button">
+        <button onClick={() => {onChangingSelectedEditEvent(eventId);}}>Edit Button</button>
+      </div>
     </div>
   );
 }
+
 
 AdminEvent.propTypes = {
   onChangingSelectedEditEvent: PropTypes.func,
