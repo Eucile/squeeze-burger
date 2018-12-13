3ba4e19449d00cb4cb7bffbb388db4e9eb9a5d99
Brendan Shea <bjrshea@gmail.com>
2018-12-12 20:31:32 -0800

    Add date, time open, and time close to edit form component

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -5,22 +5,30 @@ import PropTypes from 'prop-types';
 
 function EditForm(props) {
 
+  console.log(props)
+
   let _streetAddress = null;
   let _city = null;
   let _addressState = null;
   let _zipcode = null;
+  let _date = null;
+  let _timeOpen = null;
+  let _timeClose = null;
 
   if(props.selectedEditEvent != null) {
     let _streetAddress = props.streetAddressInput;
     let _city = props.cityInput;
     let _addressState = props.addressStateInput;
     let _zipcode = props.zipcodeInput;
+    let _date = props.dateInput;
+    let _timeOpen = props.timeOpenInput;
+    let _timeClose = props.timeCloseInput;
   }
 
   function handleEditAddressSubmission(event) {
     const { dispatch } = props;
     event.preventDefault();
-    dispatch(editSelectedAddress(props.selectedEditEvent, _streetAddress.value, _city.value, _addressState.value, _zipcode.value));
+    dispatch(editSelectedAddress(props.selectedEditEvent, _streetAddress.value, _city.value, _addressState.value, _zipcode.value, _date.value, _timeOpen.value, _timeClose.value));
   };
 
   function handleDeleteAddressSubmission(event) {
@@ -64,6 +72,30 @@ function EditForm(props) {
           value={props.zipcodeInput}
           ref={(input) => {_zipcode = input;}}
         />
+        <input
+          placeholder='Date'
+          type='date'
+          name='dateInput'
+          onChange={(event) => {props.onInputChange(event)}}
+          value={props.zipcodeInput}
+          ref={(input) => {_date = input;}}
+        />
+        <input
+          placeholder='Time Open'
+          type='time'
+          name='timeOpenInput'
+          onChange={(event) => {props.onInputChange(event)}}
+          value={props.zipcodeInput}
+          ref={(input) => {_timeOpen = input;}}
+        />
+        <input
+          placeholder='Time Close'
+          type='time'
+          name='timeCloseInput'
+          onChange={(event) => {props.onInputChange(event)}}
+          value={props.zipcodeInput}
+          ref={(input) => {_timeClose = input;}}
+        />
         <button onClick={handleEditAddressSubmission}>Submit</button>
         <button onClick={handleDeleteAddressSubmission}>Delete</button>
       </form>
@@ -78,7 +110,10 @@ EditForm.propTypes = {
   streetAddressInput: PropTypes.string,
   cityInput: PropTypes.string,
   addressStateInput: PropTypes.string,
-  zipcodeInput: PropTypes.string
+  zipcodeInput: PropTypes.string,
+  dateInput: PropTypes.string,
+  timeOpenInput: PropTypes.number,
+  timeCloseInput: PropTypes.number
 };
 
 export default connect()(EditForm);
